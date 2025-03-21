91645112737645e138595f7231794106fce2e8d3
enderrazer <enderrazer@mail.ru>
2025-03-21 12:26:19 +0300

    Reorganize project file structure and clean up code

---
+++ include/classes/EnsembleManager.h
@@ -0,0 +1,252 @@
+#ifndef ENSEMBLE_MANAGER_H
+#define ENSEMBLE_MANAGER_H
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include "nlohmann/json.hpp"
+
+#include "classes/IncrementalIntegrator.h" //Интегральная функция
+#include "classes/Matrix3.h"
+#include "classes/ThreadPool.h"
+#include "classes/Vector3.h"
+#include "core/Settings.h"
+#include "core/System.h"
+#include "macroparams/Einstein.h"
+#include "macroparams/GreenKubo.h"
+
+struct Ensemble {
+  int id_;
+  int cur_; // Внутренний счетчик ансамбля
+
+  std::vector<Vector3<double>> // Начальные компоненты скорости
+      init_velocity_{{0.0, 0.0, 0.0}}; // (для расчетов коэффициента диффузии)
+  std::vector<double> accum_acfv_{0.0}; // Автокорреляционные функции скорости
+  std::vector<double> coef_diff_integral_{0.0}; // Интеграл от АКФ
+
+  Matrix3 init_pressure_tensors_{0.0}; // Начальные тензоры давления системы
+  std::vector<double> accum_acfp_{
+      0.0}; // Автокорреляционные функции тензоров давления
+  std::vector<double> coef_visc_integral_{0.0}; // Интеграл от АКФ
+};
+
+using json = nlohmann::json;
+
+class EnsembleManager {
+private:
+  bool finished_{false};
+  bool toggle_{false};
+  ThreadPool &threadPool_;
+  IncrementalIntegrator integral_diff_;
+  IncrementalIntegrator integral_visc_;
+  GreenKubo gk;
+  int nsteps_;
+  int ensemble_threads_num_; // Количество параллельных "потоков" ансамблей
+  int ensemble_size_; // Размер ансамбля (кол-во шагов)
+  int ensemble_offset_; // Отступ между ансамблями
+  int ensemble_count_; // Сколько всего полных ансамблей собирать
+
+  int current_ens_count{0};
+  std::vector<Ensemble> ensembles_;
+
+  std::vector<double> ensembles_accum_acfv_{0.0};
+  std::vector<double> ensembles_accum_acfp_{0.0};
+
+  std::vector<double> ensembles_coef_diff_integral_{0.0};
+  std::vector<double> ensembles_coef_visc_integral_{0.0};
+
+  inline void flushEnsemble(Ensemble &ens) {
+    ens.cur_ = 0;
+    for (int i = 0; i < ensemble_size_; i++) {
+      // Записываем
+      ensembles_accum_acfv_[i] += ens.accum_acfv_[i];
+      ensembles_accum_acfp_[i] += ens.accum_acfp_[i];
+      ensembles_coef_diff_integral_[i] += ens.coef_diff_integral_[i];
+      ensembles_coef_visc_integral_[i] += ens.coef_visc_integral_[i];
+      // Очищаем
+      ens.accum_acfv_[i] = 0.0;
+      ens.accum_acfp_[i] = 0.0;
+      ens.coef_diff_integral_[i] = 0.0;
+      ens.coef_visc_integral_[i] = 0.0;
+    }
+  };
+
+  inline void initEnsemble(int thread_finished_count, Ensemble &ens,
+                           System &sys) {
+    // Если не успеваем рассчитать полный ансамбль, то и не считаем, чтобы не
+    // забирать ресурсы
+    ens.id_ += thread_finished_count + current_ens_count;
+    if (sys.currentStep() + ensemble_size_ > nsteps_) {
+      ens.cur_ = -INT32_MAX; // Просто очень большое отрицательное число до
+                             // которого не дойдем
+      return;
+    }
+    const int pn = sys.particleNumber();
+    const std::vector<Particle> &particles = sys.particles();
+    for (int j = 0; j < pn; j++) {
+      ens.init_velocity_[j] = particles[j].velocity();
+    }
+    ens.init_pressure_tensors_ = sys.pressureTensors();
+  }
+
+public:
+  EnsembleManager(json &config, Settings &settings, System &sys,
+                  ThreadPool &threadPool)
+      : integral_diff_(settings.dt()), integral_visc_(settings.dt()),
+        threadPool_(threadPool) {
+    toggle_ = config.value("toggle", false);
+    if (toggle_) {
+      nsteps_ = settings.steps();
+      ensemble_threads_num_ = config.value("threads", 1);
+      ensemble_size_ = config.value("size", 1000000);
+      ensemble_offset_ = config.value("offset", 1000000);
+      ensemble_count_ = config.value("quantity", 100);
+      ensembles_.resize(ensemble_threads_num_);
+      ensembles_accum_acfv_.resize(ensemble_size_, 0.0);
+      ensembles_accum_acfp_.resize(ensemble_size_, 0.0);
+
+      ensembles_coef_diff_integral_.resize(ensemble_size_);
+      ensembles_coef_visc_integral_.resize(ensemble_size_);
+
+      int expected_contribution = 0;
+      for (int i = 0; i < ensemble_threads_num_; i++) {
+        ensembles_[i].id_ = i;
+        ensembles_[i].cur_ = -i * ensemble_offset_;
+        ensembles_[i].init_velocity_.resize(sys.particleNumber(), {0, 0, 0});
+        ensembles_[i].accum_acfv_.resize(ensemble_size_, 0.0);
+        ensembles_[i].accum_acfp_.resize(ensemble_size_, 0.0);
+        ensembles_[i].coef_diff_integral_.resize(ensemble_size_, 0.0);
+        ensembles_[i].coef_visc_integral_.resize(ensemble_size_, 0.0);
+
+        int ensemble_expected_contribution =
+            settings.steps() / (ensemble_size_ + i * ensemble_offset_);
+        expected_contribution += ensemble_expected_contribution;
+      }
+      if (expected_contribution < ensemble_count_) {
+        std::ostringstream oss;
+        oss << "With current setup accumulated ensembles will be: "
+            << std::to_string(expected_contribution) << "/"
+            << std::to_string(ensemble_count_);
+        std::cerr << oss.str() << std::endl;
+        exit(0);
+      }
+    }
+  };
+  // Запрещаем копирование
+  EnsembleManager(const EnsembleManager &) = delete;
+  EnsembleManager &operator=(const EnsembleManager &) = delete;
+
+  ~EnsembleManager() = default;
+
+  inline const bool enabled() const { return toggle_; }
+  inline const bool completed() const { return finished_; };
+
+  int getEnsembleThreadsNum() const { return ensemble_threads_num_; }
+  int getEnsembleSize() const { return ensemble_size_; }
+  int getEnsembleOffset() const { return ensemble_offset_; }
+  int getEnsembleCount() const { return ensemble_count_; }
+  int getCurrentEnsCount() const { return current_ens_count; }
+  const std::vector<Ensemble> &getEnsembles() const { return ensembles_; }
+  const std::vector<double> &getEnsemblesAccumAcfv() const {
+    return ensembles_accum_acfv_;
+  }
+  const std::vector<double> &getEnsemblesAccumAcfp() const {
+    return ensembles_accum_acfp_;
+  }
+  const std::vector<double> &getEnsemblesCoefDiffIntegral() const {
+    return ensembles_coef_diff_integral_;
+  }
+  const std::vector<double> &getEnsemblesCoefViscIntegral() const {
+    return ensembles_coef_visc_integral_;
+  }
+  std::string getData() const {
+    std::ostringstream oss;
+    oss << "Using ensembles to calculate transport coefficients"
+        << "\n\tКоличество параллельных потоков ансамблей: "
+        << ensemble_threads_num_
+        << "\n\tКоличество шагов в ансамбле: " << ensemble_size_
+        << "\n\tОтступ между ансамблями (шагов): " << ensemble_offset_
+        << "\n\tКоличество рассчитанных ансамблей: " << ensemble_count_
+        << "\n\tEnsembles Size: " << ensembles_.size();
+
+    for (const auto &ensemble : ensembles_) {
+      oss << "\n\t\tID Ансамбля: " << ensemble.id_
+          << "\n\t\tТекущий шаг: " << ensemble.cur_
+          << "\n\t\tInit Velocity Size: " << ensemble.init_velocity_.size()
+          << "\n\t\tAccum ACFV Size: " << ensemble.accum_acfv_.size()
+          << "\n\t\tCoef Diff Integral Size: "
+          << ensemble.coef_diff_integral_.size()
+
+          << "\n\t\tAccum ACFP Size: " << ensemble.accum_acfp_.size()
+          << "\n\t\tCoef Visc Integral Size: "
+          << ensemble.coef_visc_integral_.size() << "\n";
+    }
+    return oss.str();
+  }
+
+  void accumulateGreenCubo(System &sys) {
+    if (current_ens_count < ensemble_count_) {
+      const int blockSize =
+          ensemble_threads_num_ /
+          threadPool_.getThreads(); // или любая удобная величина
+      std::vector<std::future<int>> futures;
+
+      for (int start = 0; start < ensemble_threads_num_; start += blockSize) {
+        int end = std::min(start + blockSize, ensemble_threads_num_);
+        // enqueue задачу на вычисление для блока [start, end)
+        futures.push_back(threadPool_.enqueue([this, &sys, start, end]() {
+          int thread_finished_count = 0;
+          for (int i = start; i < end; i++) {
+            Ensemble &ens = ensembles_[i];
+            // У ансамблей есть "задержка" начала расчетов, если еще не
+            // дошли то просто увеличиваем текущий шаг ансамбля
+            if (ens.cur_ < 0) {
+              ens.cur_++;
+              continue;
+            }
+            // Если ансамбль полностью заполнен, записываем в усредненные
+            // значения и обнуляем
+            if (ens.cur_ >= ensemble_size_) {
+              flushEnsemble(ens);
+              thread_finished_count++;
+            }
+            // Если текущий шаг 0, то инициализируем ансамбль
+            if (ens.cur_ == 0) {
+              initEnsemble(thread_finished_count, ens, sys);
+            }
+            // Диффузия
+            double acfv = gk.getACFV(sys, ens.init_velocity_);
+            ens.accum_acfv_[ens.cur_] = acfv;
+            ens.coef_diff_integral_[ens.cur_] =
+                integral_diff_.add_partial(acfv) / (3 * sys.particleNumber());
+            // Вязкость
+            double acfp = gk.getACFP(sys, ens.init_pressure_tensors_);
+            ens.accum_acfp_[ens.cur_] = acfp;
+            ens.coef_visc_integral_[ens.cur_] =
+                integral_visc_.add_partial(acfp) * sys.dimensions().volume() /
+                (3 * sys.temperature());
+
+            ens.cur_++;
+          }
+          return thread_finished_count;
+        }));
+      }
+
+      for (auto &f : futures) {
+        current_ens_count += f.get();
+      }
+    } else {
+      finished_ = true;
+      for (int i = 0; i < ensemble_size_; i++) {
+        ensembles_accum_acfp_[i] /= ensemble_count_;
+        ensembles_accum_acfv_[i] /= ensemble_count_;
+        ensembles_coef_diff_integral_[i] /= ensemble_count_;
+        ensembles_coef_visc_integral_[i] /= ensemble_count_;
+      }
+    }
+  };
+};
+
+#endif
---
+++ include/core/System.h
@@ -0,0 +1,247 @@
+#ifndef SYSTEM_H
+#define SYSTEM_H
+
+#include <iostream>
+#include <nlohmann/json.hpp>
+#include <sstream>
+#include <string>
+
+#include "classes/Dimensions.h" //Размеры куба
+#include "classes/Energy.h"     //Энергии
+#include "classes/Particle.h"   //Класс частицы
+
+#include "core/Settings.h" //Класс параметров
+
+using json = nlohmann::json;
+
+class System {
+private:
+  int current_step_{0}; // Текущий шаг моделирования
+
+  int particle_number_{0}; // Количество частиц
+
+  Dimensions dimensions_{};           // Размеры системы
+  std::vector<Particle> particles_{}; // Частицы
+
+  Energy energies_{}; // Усредненные энергии системы на 1 частицу
+  Energy energies_avg_{}; // Усредненные энергии системы по шагам
+
+  double temperature_{0.0},
+      temperature_avg_{0.0}; // Температура моментальная/усредненная по шагам
+  double pressure_{0.0},
+      pressure_avg_{0.0}; // Давление моментальное/усредненное по шагам
+  Matrix3 pressure_tensors_{};
+
+  Vector3<double> vcm_{}; // Скорость центра масс
+  double pulse_{0.0};     // Импульс системы
+
+  inline void initializeParticles(double mass) {
+    particles_.reserve(particle_number_);
+    for (int i = 0; i < particle_number_; ++i) {
+      particles_.emplace_back(mass);
+    }
+  }
+
+public:
+  System() = default;
+  // Конструкторы / Деструкторы
+  System(const json &config, const Settings &settings)
+      : dimensions_(config.at("dimensions")) {
+    if (settings.structType() == std::string("CP")) {
+      if (settings.hasPbc()) {
+        particle_number_ = dimensions_.numCristX() * dimensions_.numCristY() *
+                           dimensions_.numCristZ();
+      } else {
+        particle_number_ = (dimensions_.numCristX() + 1) *
+                           (dimensions_.numCristY() + 1) *
+                           (dimensions_.numCristZ() + 1);
+      }
+    } else if (settings.structType() == std::string("FCC")) {
+      if (settings.hasPbc()) {
+        particle_number_ = 4 * dimensions_.numCristX() *
+                           dimensions_.numCristY() * dimensions_.numCristZ();
+      } else {
+        particle_number_ =
+            (dimensions_.numCristX() + 1) * (dimensions_.numCristY() + 1) *
+                (dimensions_.numCristZ() + 1) +
+            (3 * dimensions_.numCristX() * dimensions_.numCristY() *
+             (dimensions_.numCristZ() + 1));
+      }
+    } else if (settings.structType() == std::string("Custom")) {
+      particle_number_ = settings.customLayout().at("particles").size();
+    } else {
+      throw std::runtime_error("Unknown structure type: " +
+                               settings.structType());
+    }
+    // Предзаполнение массива частиц
+    initializeParticles(settings.mass());
+  }
+  ~System() = default;
+
+  // Запрещаем копирование
+  System(const System &) = delete;
+  System &operator=(const System &) = delete;
+
+  // Сеттеры и геттеры
+  inline int currentStep() const { return current_step_; }
+  inline void setCurrentStep(int step) { current_step_ = step; }
+
+  inline int particleNumber() const { return particle_number_; }
+  inline void setParticleNumber(int number) { particle_number_ = number; }
+
+  inline Dimensions &dimensions() { return dimensions_; }
+  inline const Dimensions &dimensions() const { return dimensions_; }
+  inline void setDimensions(const Dimensions &dims) { dimensions_ = dims; }
+
+  inline std::vector<Particle> &particles() { return particles_; }
+  inline const std::vector<Particle> &particles() const { return particles_; }
+  inline void setParticles(std::vector<Particle> parts) {
+    particles_ = std::move(parts);
+  }
+
+  inline Energy &energies() { return energies_; }
+  inline const Energy &energies() const { return energies_; }
+  inline void setEnergies(const Energy &energy) { energies_ = energy; }
+
+  inline Energy &energiesAvg() { return energies_avg_; }
+  inline const Energy &energiesAvg() const { return energies_avg_; }
+  inline void setEnergiesAvg(const Energy &energy_avg) {
+    energies_avg_ = energy_avg;
+  }
+
+  inline double temperature() const { return temperature_; }
+  inline void setTemperature(double temp) { temperature_ = temp; }
+
+  inline double temperatureAvg() const { return temperature_avg_; }
+  inline void setTemperatureAvg(double temp_avg) {
+    temperature_avg_ = temp_avg;
+  }
+  inline void updateTemperatureAvg() {
+    temperature_avg_ =
+        ((temperature_avg_ * (current_step_ - 1)) + temperature_) /
+        current_step_;
+  }
+  inline Matrix3 pressureTensors() { return pressure_tensors_; };
+  inline Matrix3 pressureTensors() const { return pressure_tensors_; };
+  inline void setPressureTensors(Matrix3 p_t) { pressure_tensors_ = p_t; };
+
+  inline double pressure() const { return pressure_; }
+  inline void setPressure(double pres) { pressure_ = pres; }
+
+  inline double pressureAvg() const { return pressure_avg_; }
+  inline void setPressureAvg(double pres_avg) { pressure_avg_ = pres_avg; }
+  inline void updatePressureAvg() {
+    pressure_avg_ =
+        ((pressure_avg_ * (current_step_ - 1)) + pressure_) / current_step_;
+  }
+
+  inline const Vector3<double> &vcm() const { return vcm_; }
+  inline void setVcm(const Vector3<double> &v) { vcm_ = v; }
+
+  inline double pulse() const { return pulse_; }
+  inline void setPulse(double p) { pulse_ = p; }
+  // Методы
+  void updateEnergy() {
+    double e_pot = 0, e_kin = 0, e_term = 0, e_int = 0, e_full = 0;
+    for (auto &p : particles_) {
+      p.updateEnergy(vcm_);
+      e_pot += p.energy(Energy::EnergyType::Potential);
+      e_kin += p.energy(Energy::EnergyType::Kinetic);
+      e_term += p.energy(Energy::EnergyType::Thermodynamic);
+      e_int += p.energy(Energy::EnergyType::Internal);
+      e_full += p.energy(Energy::EnergyType::Full);
+    }
+    // Средняя на 1 частицу
+    energies_.set(Energy::EnergyType::Potential, e_pot / particle_number_);
+    energies_.set(Energy::EnergyType::Kinetic, e_kin / particle_number_);
+    energies_.set(Energy::EnergyType::Thermodynamic, e_term / particle_number_);
+    energies_.set(Energy::EnergyType::Internal, e_int / particle_number_);
+    energies_.set(Energy::EnergyType::Full, e_full / particle_number_);
+  }
+  void updateEnergyAvg() {
+    int prev_factor = (current_step_ - 1) / current_step_;
+    double curr_factor = 1.0 / current_step_;
+
+    for (Energy::EnergyType type :
+         {Energy::EnergyType::Potential, Energy::EnergyType::Kinetic,
+          Energy::EnergyType::Thermodynamic, Energy::EnergyType::Internal,
+          Energy::EnergyType::Full}) {
+      double prev_avg = energies_avg_.get(type);
+      double curr_value = energies_.get(type);
+
+      energies_avg_.set(type,
+                        prev_avg * prev_factor + curr_value * curr_factor);
+    }
+  }
+  void updateVCM() {
+    Vector3<double> vel;
+    for (Particle &p : particles_) {
+      vel += p.velocity();
+    }
+    vcm_ = vel / particle_number_;
+  }
+  void updatePulse() {
+    double pulse = 0.0;
+    for (Particle &p : particles_) {
+      pulse += p.pulse();
+    }
+    pulse_ = pulse / particle_number_;
+  }
+
+  inline void advanceStep() { current_step_++; }
+
+  // Output methods
+  std::string getData() const {
+    std::ostringstream oss;
+    oss.precision(16);
+    oss << dimensions_.getData() << "\nParticle number: " << particle_number_
+        << "\nCurrent step: " << current_step_
+        << "\nTemperature: " << temperature_ << "\nPressure: " << pressure_
+        << "\n";
+
+    return oss.str();
+  }
+  std::string getShortData() const {
+    std::ostringstream oss;
+    oss.precision(16);
+    oss << "Current step: " << current_step_ << "\nMomentum:"
+        << "\n\tTemperature: " << temperature_ << "\n\tPressure: " << pressure_
+        << "\nAverage:"
+        << "\n\tTemperature: " << temperature_avg_
+        << "\n\tPressure: " << pressure_avg_ << "\n";
+
+    return oss.str();
+  }
+  std::string getParticlesInfo() const {
+    std::ostringstream oss;
+    oss.precision(16);
+    oss << "Частицы: " << particle_number_ << std::endl;
+    for (const auto &particle : particles_) {
+      oss << particle << std::endl;
+    }
+
+    return oss.str();
+  }
+  std::string getParticlesCoordsInfo() const {
+    std::ostringstream oss;
+    oss.precision(16);
+    oss << "Coordinates:\n" << std::endl;
+    for (const auto &particle : particles_) {
+      oss << particle.coord() << std::endl;
+    }
+
+    return oss.str();
+  }
+  std::string getParticlesVelocityInfo() const {
+    std::ostringstream oss;
+    oss.precision(16);
+    oss << "Velocity:\n" << std::endl;
+    for (const auto &particle : particles_) {
+      oss << particle.velocity() << std::endl;
+    }
+
+    return oss.str();
+  }
+};
+
+#endif // SYSTEM_H
