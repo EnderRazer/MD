fb0b41ca6d9225c86833be6a090274d0f6291d96
enderrazer <enderrazer@mail.ru>
2025-04-25 04:09:48 +0300

    Wrap main function in block scope to improve memory cleanup

--- Usachev_md.cpp
+++ Usachev_md.cpp
@@ -64,131 +64,133 @@ std::unique_ptr<Barostat> createBarostat(const json &config,
 }
 
 int main(int argc, char *argv[]) {
-  Timer timer(0);
-  timer.start();
-  // Проверка аргументов командной строки для имени конфигурационного файла
-  if (argc < 2) {
-    std::cerr << "Использование: " << argv[0] << " config.json\n";
-    return 1;
-  }
-  std::ifstream configFile(argv[1]);
-  if (!configFile) {
-    std::cerr << "Не удалось открыть файл конфигурации.\n";
-    return 1;
-  }
-  // Считали конфигурацию
-  json config;
-  configFile >> config;
-  configFile.close();
-
-  // Настройки запуска
-  Settings settings(config); // Настройки системы
-  cout << settings.getData() << endl;
-
-  // Создаем пул потоков
-  ThreadPool threadPool(settings.threads());
-  cout << threadPool.getData() << endl;
-
-  // Заводим переменную System, которая хранит все состояние системы
-  System sys = System(config, settings);
-  cout << sys.getData() << endl;
-
-  // Заводим переменную Backup для бекапов
-  BackupManager backupManager = BackupManager(config["backup"], settings);
-  cout << backupManager.getData() << endl;
-  if (!backupManager.enabled()) {
-    // Генерация начальных координат и скоростей
-    Generator generator = Generator(settings);
-    generator.generateCoords(sys, settings);
-    if (settings.structType() != std::string("Custom"))
-      generator.generateSpeeds(sys, settings);
-  } else {
-    if (backupManager.restoreBackup(sys))
-      cout << "Восстановление из резервной копии прошло успешно!" << endl;
-    else {
-      exit(0);
+  {
+    Timer timer(0);
+    timer.start();
+    // Проверка аргументов командной строки для имени конфигурационного файла
+    if (argc < 2) {
+      std::cerr << "Использование: " << argv[0] << " config.json\n";
+      return 1;
     }
-  }
+    std::ifstream configFile(argv[1]);
+    if (!configFile) {
+      std::cerr << "Не удалось открыть файл конфигурации.\n";
+      return 1;
+    }
+    // Считали конфигурацию
+    json config;
+    configFile >> config;
+    configFile.close();
 
-  // Создаём потенциал, термостат и баростат через фабрики:
+    // Настройки запуска
+    Settings settings(config); // Настройки системы
+    cout << settings.getData() << endl;
 
-  // Заводим потенциал (обязателен)
-  std::unique_ptr<Potential> potential{nullptr};
-  if (!config.contains("potential"))
-    throw std::invalid_argument(
-        "В файле конфигурации нет настроек используемого потенциала");
-  potential = createPotential(config["potential"]);
-  cout << potential->getData() << endl;
+    // Создаем пул потоков
+    ThreadPool threadPool(settings.threads());
+    cout << threadPool.getData() << endl;
 
-  // Заводим термостат, если включен
-  std::unique_ptr<Thermostat> thermostat{nullptr};
-  if (config.contains("thermostat") &&
-      config["thermostat"].value("toggle", false)) {
-    json thermo_config = config["thermostat"];
-    thermostat =
-        createThermostat(thermo_config, settings, sys.particleNumber());
-  }
-  if (thermostat)
-    cout << thermostat->getData() << endl;
-  else
-    cout << "Термостат выключен" << endl;
+    // Заводим переменную System, которая хранит все состояние системы
+    System sys = System(config, settings);
+    cout << sys.getData() << endl;
 
-  // Заводим баростат, если включен
-  std::unique_ptr<Barostat> barostat{nullptr};
-  if (config.contains("barostat") &&
-      config["barostat"].value("toggle", false)) {
-    json baro_config = config["barostat"];
-    barostat = createBarostat(baro_config, settings);
-  }
-  if (barostat)
-    cout << barostat->getData();
-  else
-    cout << "Баростат выключен" << endl;
+    // Заводим переменную Backup для бекапов
+    BackupManager backupManager = BackupManager(config["backup"], settings);
+    cout << backupManager.getData() << endl;
+    if (!backupManager.enabled()) {
+      // Генерация начальных координат и скоростей
+      Generator generator = Generator(settings);
+      generator.generateCoords(sys, settings);
+      if (settings.structType() != std::string("Custom"))
+        generator.generateSpeeds(sys, settings);
+    } else {
+      if (backupManager.restoreBackup(sys))
+        cout << "Восстановление из резервной копии прошло успешно!" << endl;
+      else {
+        exit(0);
+      }
+    }
 
-  // Заводим переменную Output для вывода в файлы/консоль
-  OutputManager outputManager = OutputManager(config["output"], settings);
-  cout << outputManager.getData() << endl;
+    // Создаём потенциал, термостат и баростат через фабрики:
 
-  // Заводим переменную EnsembleManager для управления ансамблями
-  std::unique_ptr<EnsembleManager> ensembleManager{nullptr};
-  if (config["ensemble"].value("toggle", false)) {
-    ensembleManager = std::make_unique<EnsembleManager>(
-        config["ensemble"], settings, sys, threadPool, outputManager);
-    cout << ensembleManager->getData() << endl;
-  }
+    // Заводим потенциал (обязателен)
+    std::unique_ptr<Potential> potential{nullptr};
+    if (!config.contains("potential"))
+      throw std::invalid_argument(
+          "В файле конфигурации нет настроек используемого потенциала");
+    potential = createPotential(config["potential"]);
+    cout << potential->getData() << endl;
 
-  // Заводим переменную MDAlgorimths, которая содержит все методы МД
-  json macroparams_config = config["macroparams"];
-  MDAlgorithms md = MDAlgorithms(settings, sys, backupManager, outputManager,
-                                 threadPool, std::move(ensembleManager),
-                                 std::move(potential), std::move(thermostat),
-                                 std::move(barostat), macroparams_config);
+    // Заводим термостат, если включен
+    std::unique_ptr<Thermostat> thermostat{nullptr};
+    if (config.contains("thermostat") &&
+        config["thermostat"].value("toggle", false)) {
+      json thermo_config = config["thermostat"];
+      thermostat =
+          createThermostat(thermo_config, settings, sys.particleNumber());
+    }
+    if (thermostat)
+      cout << thermostat->getData() << endl;
+    else
+      cout << "Термостат выключен" << endl;
 
-  // Записываем конфигурацию запуска
-  outputManager.writeModellingProperties(config);
+    // Заводим баростат, если включен
+    std::unique_ptr<Barostat> barostat{nullptr};
+    if (config.contains("barostat") &&
+        config["barostat"].value("toggle", false)) {
+      json baro_config = config["barostat"];
+      barostat = createBarostat(baro_config, settings);
+    }
+    if (barostat)
+      cout << barostat->getData();
+    else
+      cout << "Баростат выключен" << endl;
 
-  //================
-  // НАЧАЛО РАСЧЕТОВ
-  cout << "Начало расчета!" << endl;
-  Timer step_timer(0); // Таймер для отслеживания времени выполнения шагов
-  step_timer.start();
-  md.initialStep(sys); // Начальный (нулевой) шаг
-  step_timer.stop();
-  cout << sys.getShortData() << endl;
-  cout << "Init step done in " << step_timer.elapsed() << endl;
+    // Заводим переменную Output для вывода в файлы/консоль
+    OutputManager outputManager = OutputManager(config["output"], settings);
+    cout << outputManager.getData() << endl;
 
-  bool completed = false;
-  // Основной цикл МД
-  while (!completed && sys.currentStep() < settings.steps() + 1) {
+    // Заводим переменную EnsembleManager для управления ансамблями
+    std::unique_ptr<EnsembleManager> ensembleManager{nullptr};
+    if (config["ensemble"].value("toggle", false)) {
+      ensembleManager = std::make_unique<EnsembleManager>(
+          config["ensemble"], settings, sys, threadPool, outputManager);
+      cout << ensembleManager->getData() << endl;
+    }
+
+    // Заводим переменную MDAlgorimths, которая содержит все методы МД
+    json macroparams_config = config["macroparams"];
+    MDAlgorithms md = MDAlgorithms(settings, sys, backupManager, outputManager,
+                                   threadPool, std::move(ensembleManager),
+                                   std::move(potential), std::move(thermostat),
+                                   std::move(barostat), macroparams_config);
+
+    // Записываем конфигурацию запуска
+    outputManager.writeModellingProperties(config);
+
+    //================
+    // НАЧАЛО РАСЧЕТОВ
+    cout << "Начало расчета!" << endl;
+    Timer step_timer(0); // Таймер для отслеживания времени выполнения шагов
     step_timer.start();
-    completed = md.advanceStep(sys);
+    md.initialStep(sys); // Начальный (нулевой) шаг
     step_timer.stop();
     cout << sys.getShortData() << endl;
-    cout << "Step " << sys.currentStep() << " - done in "
-         << step_timer.elapsed() << endl;
+    cout << "Init step done in " << step_timer.elapsed() << endl;
+
+    bool completed = false;
+    // Основной цикл МД
+    while (!completed && sys.currentStep() < settings.steps()) {
+      step_timer.start();
+      completed = md.advanceStep(sys);
+      step_timer.stop();
+      cout << sys.getShortData() << endl;
+      cout << "Step " << sys.currentStep() << " - done in "
+           << step_timer.elapsed() << endl;
+    }
+    timer.stop();
+    cout << "Time elapsed: " << timer.elapsed() << endl;
+    cout << "Завершение расчета" << endl;
   }
-  timer.stop();
-  cout << "Time elapsed: " << timer.elapsed() << endl;
-  cout << "Завершение расчета" << endl;
   return 0;
 }
